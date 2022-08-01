d1b30acb222b55769d6727659fde485f842fa266
Fares Kissoum <kissoumfares@gmail.com>
2022-08-01 23:26:43 +0100

    well see

--- Exercise1/exercise1.py
+++ Exercise1/exercise1.py
@@ -3,108 +3,59 @@ import os
 
 # Scientific and vector computation for python
 import numpy as np
-
-# Plotting library
-from matplotlib import pyplot
-from mpl_toolkits.mplot3d import Axes3D  # needed to plot 3-D surfaces
-
-# Read comma separated data
-data = np.loadtxt(
-    os.path.join('/Users/fareskissoum/Documents/Personal-Projects.tmp/ml-coursera-python-assignments/Exercise1/Data',
-                 'ex1data1.txt'), delimiter=',')
-X, y = data[:, 0], data[:, 1]
-
-m = y.size  # number of training examples
-
-
-def plotData(x, y):
+def featureNormalize(X):
     """
-    Plots the data points x and y into a new figure. Plots the data
-    points and gives the figure axes labels of population and profit.
-
-    Parameters
-    ----------
-    x : array_like
-        Data point values for x-axis.
-
-    y : array_like
-        Data point values for y-axis. Note x and y should have the same size.
-
-    Instructions
-    ------------
-    Plot the training data into a figure using the "figure" and "plot"
-    functions. Set the axes labels using the "xlabel" and "ylabel" functions.
-    Assume the population and revenue data have been passed in as the x
-    and y arguments of this function.
-
-    Hint
-    ----
-    You can use the 'ro' option with plot to have the markers
-    appear as red circles. Furthermore, you can make the markers larger by
-    using plot(..., 'ro', ms=10), where `ms` refers to marker size. You
-    can also set the marker edge color using the `mec` property.
-    """
-
-    fig = pyplot.figure()  # open a new figure
-
-    # ====================== YOUR CODE HERE =======================
-    pyplot.plot(x, y, 'ro', ms=10, mec='k')
-    pyplot.xlabel('Population of City in 10,000s')
-    pyplot.ylabel('Profit in $10,000s')
-    # =============================================================
-
-
-X = np.stack([np.ones(m), X], axis=1)
-
-
-def compute_cost(X, y, theta):
-    """
-    Compute cost for linear regression. Computes the cost of using theta as the
-    parameter for linear regression to fit the data points in X and y.
-
+    Normalizes the features in X. returns a normalized version of X where
+    the mean value of each feature is 0 and the standard deviation
+    is 1. This is often a good preprocessing step to do when working with
+    learning algorithms.
+    
     Parameters
     ----------
     X : array_like
-        The input dataset of shape (m x n+1), where m is the number of examples,
-        and n is the number of features. We assume a vector of one's already
-        appended to the features so we have n+1 columns.
-
-    y : array_like
-        The values of the function at each data point. This is a vector of
-        shape (m, ).
-
-    theta : array_like
-        The parameters for the regression function. This is a vector of
-        shape (n+1, ).
-
+        The dataset of shape (m x n).
+    
     Returns
     -------
-    J : float
-        The value of the regression cost function.
-
+    X_norm : array_like
+        The normalized dataset of shape (m x n).
+    
     Instructions
     ------------
-    Compute the cost of a particular choice of theta.
-    You should set J to the cost.
+    First, for each feature dimension, compute the mean of the feature
+    and subtract it from the dataset, storing the mean value in mu. 
+    Next, compute the  standard deviation of each feature and divide
+    each feature by it's standard deviation, storing the standard deviation 
+    in sigma. 
+    
+    Note that X is a matrix where each column is a feature and each row is
+    an example. You needto perform the normalization separately for each feature. 
+    
+    Hint
+    ----
+    You might find the 'np.mean' and 'np.std' functions useful.
     """
+    # You need to set these values correctly
+    X_norm = X.copy()
+    mu = np.zeros(X.shape[1])
+    sigma = np.zeros(X.shape[1])
+    # =========================== YOUR CODE HERE =====================
+    num_rows, num_cols = X_norm.shape
+    for i in range(num_cols):
+        mean =  X[:,i].mean()
+        std =  X[:,i].std()
+        mu[:,i] = mean
+        sigma[:,i] = std
+        X_norm[:,i] = (X[:,i] - mean)/std
+    
+    # ================================================================
+    return X_norm, mu, sigma
 
-    # initialize some useful values
-    m = y.size  # number of training examples
+data = np.loadtxt(os.path.join('/Users/fareskissoum/Documents/Personal-Projects.tmp/ml-coursera-python-assignments/Exercise1/Data', 'ex1data2.txt'), delimiter=',')
+X = data[:, :2]
+y = data[:, 2]
+m = y.size
+X_norm, mu, sigma = featureNormalize(X)
 
-    # You need to return the following variables correctly
-    J = 0
-    thing = X.dot(theta)
-    # ====================== YOUR CODE HERE =====================
-    J = np.sum((X.dot(theta) - y) ** 2) / 2 * m
-    # ===========================================================
-    return J
-
-
-J = compute_cost(X, y, theta=np.array([0.0, 0.0]))
-print('With theta = [0, 0] \nCost computed = %.2f' % J)
-print('Expected cost value (approximately) 32.07\n')
-
-# further testing of the cost function
-J = compute_cost(X, y, theta=np.array([-1, 2]))
-print('With theta = [-1, 2]\nCost computed = %.2f' % J)
-print('Expected cost value (approximately) 54.24')
+print('Computed mean:', mu)
+print('Computed standard deviation:', sigma)
